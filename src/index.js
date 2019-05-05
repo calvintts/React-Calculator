6df924955e28f036c83799425be315306e54418b
calvintts <calvinteytaisoon@gmail.com>
2019-05-04 17:58:29 -0700

    DONE

--- src/index.js
+++ src/index.js
@@ -1,69 +1,81 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Output from './Output';
+import Output from './React-Calculator/consolePanel/Output';
 import Allbuttons from './Allbuttons';
 import './container.css';
 const initialState = {
-      outputValue:"0",
-      calculateValue:[],
-      currentOperator:"",
+        outputValue:0,
+        currentValue:0,
+        calculateValue:0,
+        currentOperator:"",
     }
+    const operators = "+-x÷"
 class App extends Component {
     state = initialState
-    handleCalculate = (x) => {
-    const operator = "+-x÷"
-    console.log(this.state.calculateValue)
-    if(operator.includes(x))
-    {
-      const currentInput = [...this.state.calculateValue]
-      if(operator.includes(currentInput[currentInput.length-1]))
+    handleCalculate = (val2,val1,ops) => {
+        console.log(ops)
+        console.log(typeof(ops))
+        return ops=="x"?val1*val2:ops=="÷"?val1/val2:ops=="+"?val1+val2:val1-val2
+    }
+    handleClick = (x) => {
+        console.log(this.state)
+       if(!isNaN(x)||x==".")
        {
-         currentInput.splice(-1,1,x)
+           this.setState({
+               outputValue:this.state.outputValue==0?`${x}`:`${this.state.outputValue.concat(x)}`,
+               currentValue:this.state.outputValue==0?`${x}`:`${this.state.outputValue.concat(x)}`,
+           })
        }
-       else{
-         x=x=="x"?"*":x=="÷"?"/":x
-         currentInput.push(this.state.outputValue)
-         currentInput.push(x)
+       if(operators.includes(x))
+       {
+           if(this.state.currentValue!=0&&this.state.calculateValue!=0)
+           {
+               const tempValue = new Number(this.state.outputValue)
+               this.setState({
+                   currentValue:tempValue,
+                   outputValue: 0,
+                   currentOperator:x,
+               })
+           }
+           else
+           {
+               this.setState({
+                       calculateValue:new Number(this.state.currentValue),
+                        outputValue:0,
+                        currentOperator:x,
+                   }
+               )
+           }
        }
+       if(x=="=")
+       {
+           if(this.state.currentValue!=0&&this.state.calculateValue!=0)
+           {
+               this.setState({
+                   calculateValue:this.handleCalculate(parseFloat(this.state.currentValue), parseFloat(this.state.calculateValue), this.state.currentOperator),
+                   outputValue: this.handleCalculate(parseFloat(this.state.currentValue), parseFloat(this.state.calculateValue), this.state.currentOperator),
+               })
+           }
 
-      this.setState({
-        currentOperator:x,
-        calculateValue:currentInput,
-        outputValue:"",})
-    }
-    if(!isNaN(x)||x===".")
-    {
-      this.setState({
-        outputValue:this.state.outputValue==="0"?`${x}`:`${this.state.outputValue}${x}`,
-      })
-    }
-    if(x=='=')
-    {
-      let op =  this.state.currentOperator
-      let beforeValue = this.state.calculateValue
-      beforeValue.push(this.state.outputValue)
-      if("+-*/".includes(beforeValue[-1]))
-        beforeValue.splice(-1,1)
-      console.warn(beforeValue)
-      let realValue = beforeValue.join("")=="0/0"?0:eval(beforeValue.join(""))
-      this.setState({
-        calculateValue:[],
-        outputValue:parseFloat(realValue),
-      })
-    }
-    if(x=='+/-')
-    {
-      this.setState({
-        outputValue:this.state.outputValue*-1,
-      })
-    }
-    if(x=='C')
-    {
-      this.setState(
-        initialState
-      )
-    }
-
+       }
+       if(x=="+/-")
+       {
+           this.setState({
+               calculateValue:this.state.outputValue*-1,
+               outputValue: this.state.outputValue*-1,
+           })
+       }
+       if(x=="C")
+        {
+            this.setState(initialState)
+        }
+        if(x=="%")
+        {
+            this.setState({
+                calculateValue:this.state.outputValue/100,
+                outputValue: this.state.outputValue/100,
+            })
+        }
   }
 
 
@@ -73,7 +85,7 @@ class App extends Component {
         <h1>Calvin's First React App</h1>
         <div className="container">
           <Output showVal={this.state.outputValue}/>
-          <Allbuttons calculate={this.handleCalculate}/>
+          <Allbuttons calculate={this.handleClick}/>
         </div>
       </div>
     );
