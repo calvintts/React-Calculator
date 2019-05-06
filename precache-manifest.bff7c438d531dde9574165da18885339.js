6826dd9f29ad7da13b7fdb7469753e8fe011f9f8
calvintts <calvinteytaisoon@gmail.com>
2019-05-05 22:18:09 -0700

    Updates

---
+++ precache-manifest.bff7c438d531dde9574165da18885339.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8589feb176041e96774046de85c3c241",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "3893bc71b6b1c506e1c2",
+    "url": "/React-Calculator/static/css/main.50e105f0.chunk.css"
+  },
+  {
+    "revision": "8364e01c2685ebbb9a9a",
+    "url": "/React-Calculator/static/js/2.9d401577.chunk.js"
+  },
+  {
+    "revision": "3893bc71b6b1c506e1c2",
+    "url": "/React-Calculator/static/js/main.a93071ab.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
